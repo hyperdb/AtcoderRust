3815443287b9d69e62241267ece0b501ac8cd151
hyperdb <hyperdb@gmail.com>
2024-06-10 16:14:43 +0900

    🩹ABC-042-A 判定方法を変更

--- ABC/042-100/042-050/ABC-042-A/src/main.rs
+++ ABC/042-100/042-050/ABC-042-A/src/main.rs
@@ -1,25 +1,16 @@
 use proconio::input;
 
 fn main() {
-
     input! {
         mut a:[usize; 3],
     }
-
     a.sort();
 
-    let mut r : bool = false;
-    let t: usize = a.iter().sum();
-
-    if t == 17
-    {
-        if a[0] == 5 && a[1] == 5
-        {
-            r = true;
-        }
+    let mut r: bool = false;
+    if a.eq(&[5, 5, 7]) {
+        r = true;
     }
-
-    let ans = if r {"YES"} else {"NO"};
+    let ans = if r { "YES" } else { "NO" };
 
     println!("{}", ans);
 }
